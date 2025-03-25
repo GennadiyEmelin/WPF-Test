6209b32323be6d5c9186ef4105fc115424f0a8c6
GenaCrok <gennadiyemelin92@gmail.com>
2025-03-25 22:28:05 +0300

    Second commit

---
+++ MSSQL.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace WPF
+{
+    internal class MSSQL
+    {
+        /// <summary>
+        /// Подключение к БД и запись данных в БД
+        /// </summary>
+        /// <param name="Name">Имя работника</param>
+        /// <param name="Surname">Фамилия работника</param>
+        /// <param name="Age">Возраст</param>
+        /// <param name="Salary">Зарплата</param>
+        /// <param name="Department">Департамент</param>
+        /// <param name="Post">Должность</param>
+        /// <param name="PhoneNumber">Номер телефона</param>
+        /// <param name="Passport">Паспортные данные</param>
+        public void AddSQL(string Name, string Surname, int Age, int Salary, string Department,
+            string Post, string PhoneNumber, string Passport)
+        {
+            SqlConnectionStringBuilder strCon = new SqlConnectionStringBuilder() 
+            {
+                DataSource = @"(localdb)\MSSQLLocalDB", // Данные от БД
+                InitialCatalog = "WorkersDB",
+                IntegratedSecurity = true
+            };
+            SqlConnection sqlConnection = new SqlConnection()
+            {
+                ConnectionString = strCon.ConnectionString,
+            };
+            sqlConnection.StateChange += (s, e) =>
+            {
+                Console.WriteLine($@"{nameof(sqlConnection)} " +
+                $"в состоянии: {(s as SqlConnection).State}");
+            };
+
+            try // Попытка открыть и запись
+            {
+                sqlConnection.Open();
+                string sql = $@"INSERT INTO [Table]([Name],[Surname],[Age],[Salary],[Department],[Post],[PhoneNumber],[Passport]) 
+                     values (N'{Name}',N'{Surname}',{Age},{Salary},N'{Department}',N'{Post}','{PhoneNumber}','{Passport}')";
+                SqlCommand command = new SqlCommand(sql, sqlConnection);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(sqlConnection.State);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+        /// <summary>
+        /// Чтение из БД
+        /// </summary>
+        public void SetSQL()
+        {
+            SqlConnectionStringBuilder strCon = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"(localdb)\MSSQLLocalDB", // Данные от БД
+                InitialCatalog = "WorkersDB",
+                IntegratedSecurity = true
+            };
+            SqlConnection sqlConnection = new SqlConnection()
+            {
+                ConnectionString = strCon.ConnectionString,
+            };
+            sqlConnection.StateChange += (s, e) =>
+            {
+                Console.WriteLine($@"{nameof(sqlConnection)} " +
+                $"в состоянии: {(s as SqlConnection).State}");
+            };
+
+            try // Попытка открыть и читать
+            {
+                sqlConnection.Open();
+                string sql = $@"SELECT * FROM [Table]";
+                SqlCommand command = new SqlCommand(sql, sqlConnection);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(sqlConnection.State);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace WPF
 {
@@ -20,10 +21,40 @@ namespace WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Workers> workers = new List<Workers>();
+        MSSQL _MSSQL = new MSSQL();
+        string _name;
+        string _surname;
+        int _age;
+        int _salary;
+        string _department;
+        string _post;
+        string _phoneNumber;
+        string _passport;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
 
+        private void AddWorker(object sender, RoutedEventArgs e)
+        {
+            _name = workerName.Text;
+            _surname = workerSurname.Text;
+            _age = Convert.ToInt32(workerAge.Text);
+            _salary = Convert.ToInt32(workerSalary.Text);
+            _department = workerDepartment.Text;
+            _post = workerPost.Text;
+            _phoneNumber = workerPhoneNumber.Text;
+            _passport = workerPassport.Text;
+
+            workers.Add(new Workers(_name,_surname,_age,_salary,_department,_post,_phoneNumber,_passport));
+            _MSSQL.AddSQL(_name,_surname,_age,_salary,_department,_post,_phoneNumber,_passport);
+            MessageBox.Show("Данные записаны.");
+        }
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
         }
     }
 }
