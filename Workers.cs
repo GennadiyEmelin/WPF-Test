b0f3682e21ba7c7d781775d615c77370bf72d692
GenaCrok <gennadiyemelin92@gmail.com>
2025-03-25 20:44:29 +0300

    Добавьте файлы проекта.

---
+++ Workers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF
+{
+    struct Workers
+    {
+        public string Name {  get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+        public string Post {  get; set; }
+        public string PhoneNumber{  get; set; }
+        public string Passport { get; set; }
+
+        public Workers(string Name, string Surname, int Age, int Salary, string Department, string Post, string PhoneNumber, string Passport)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Age = Age;
+            this.Salary = Salary;
+            this.Department = Department;
+            this.Post = Post;
+            this.PhoneNumber = PhoneNumber;
+            this.Passport = Passport;
+        }
+    }
+}
